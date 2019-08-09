087c5948eae89aca5bef1d9c6da97bda9d89cac4
krboelter <krboelter@gmail.com>
2019-08-09 00:31:18 -0600

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+let navHeader = document.querySelector(".nav-container");
+
+navHeader.addEventListener("mouseenter", (event) => {
+    event.target.style.backgroundColor = "yellow";
+
+    setTimeout(function() {
+        event.target.style.backgroundColor = "";
+        }, 1000);
+});
