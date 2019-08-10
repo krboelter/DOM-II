0a917830d811b5bcc5c05972dfcf5b0883b7072d
krboelter <krboelter@gmail.com>
2019-08-09 18:03:19 -0600

    Progress

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
+// ***one***
 let navHeader = document.querySelector(".nav-container");
 
-navHeader.addEventListener("mouseenter", (event) => {
+navHeader.addEventListener("mouseenter", event => {
     event.target.style.backgroundColor = "yellow";
 
     setTimeout(function() {
         event.target.style.backgroundColor = "";
         }, 1000);
 });
+
+// ***two***
+let navAnchorsArray = Array.from(document.getElementsByClassName("nav-link"));
+
+navAnchorsArray.forEach(n => n.addEventListener("mouseover", event => {
+    event.target.style.border = "3px solid green";
+    event.target.style.padding = "5px 10px";
+
+    setTimeout(function() {
+        event.target.backgroundColor = "";
+        event.target.style.border = "";
+        event.target.style.padding = "";
+    }, 1000);
+}));
+
+// ***three***
+let paragraphs = Array.from(document.getElementsByTagName("p"));
+
+paragraphs.forEach(n => n.addEventListener("click", event => {
+    event.target.classList.toggle("paragraphcolor");
+}));
+
+// ***four***
